a71be1836f0d43143a17590238443355c2a9a36a
Roman Kuchinik <kvroma83@gmail.com>
2022-03-28 22:31:22 +0300

    Add project files.

---
+++ Archive/Models/AppContext.cs
@@ -0,0 +1,25 @@
+﻿using SQLite.CodeFirst;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archive.Models
+{
+    public class AppContext : DbContext
+    {
+        public AppContext() : base("ConStr") { }
+
+        //public DbSet<Abonent> Abonents { get; set; }   // Підєднуємо таблиці моделі
+        //public DbSet<Adress> Adresse { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            var sqliteConnectionInitializer = new SqliteCreateDatabaseIfNotExists<AppContext>(modelBuilder);
+            Database.SetInitializer(sqliteConnectionInitializer);
+        }
+    }
+}
---
+++ Archive/ViewModels/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archive.ViewModels
+{
+    public class MainViewModel : ViewModel
+    {
+        public string TitleView { get; } = "Archive - 2022";
+    }
+}
